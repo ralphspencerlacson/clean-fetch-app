a524105e6f703d82803768d63ddf7f53233ed1ec
ralphspencerlacson <ralph.lacson@camayacoast.com>
2025-07-29 14:02:33 +0800

    Feature:: PRoducts and Service form initial setup

--- cfa-frontend/app/admin-dashboard/src/components/inventory/products/ProductForm.jsx
+++ cfa-frontend/app/admin-dashboard/src/components/inventory/products/ProductForm.jsx
@@ -1,10 +1,25 @@
-import { Form, Input, Button, Row, Col } from "antd";
+import { useEffect, useState } from "react";
+// Hooks
+import { useTags } from "../../../hooks/tag/useTags";
+// AntD
+import { Form, Input, InputNumber, Row, Col, Select, DatePicker, Radio } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
 
+const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 const ProductForm = () => {
-    const [form] = Form.useForm();
+    const [productForm] = Form.useForm();
+
+    const [ tagOptions, setTagOptions ] = useState([]);
+
+    const { data: tags, isLoading: tagsLoading } = useTags();
+
+    useEffect(() => {
+        if(tags && !tagsLoading) {
+            setTagOptions(tags?.data.map(tag => ({ label: tag.name, value: tag.id })));
+        }
+    }, [tags]);
 
     const handleSubmit = (values) => {
         console.log("Form values:", values);
@@ -12,56 +27,171 @@ const ProductForm = () => {
     };
 
     const handleCancel = () => {
-        form.resetFields();
+        productForm.resetFields();
         // Handle cancel logic here
     };
 
     return (
         <>
-            <Form form={form} layout="vertical" onFinish={handleSubmit}>
-                <Row gutter={[16, 0]}>
+            <Form
+                form={productForm}
+                layout="vertical"
+                onFinish={handleSubmit}
+                requiredMark={false}
+            >
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item
+                            name="date"
+                            label="Availability Date"
+                            required
+                        >
+                            <RangePicker
+                                className="w-full"
+                                format="MMMM D, YYYY"
+                                placeholder={["Start Date", "End Date"]}
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row gutter={[12, 12]}>
                     <Col xs={24} md={12}>
                         <Form.Item
                             name="code"
-                            label="Service Code"
+                            label="Product Code"
                             rules={[
                                 {
                                     required: true,
-                                    message: "Please enter service code",
+                                    message: "Please enter product code",
                                 },
                             ]}
                         >
-                            <Input placeholder="Enter service code" />
+                            <Input placeholder="Enter product code" />
                         </Form.Item>
                     </Col>
 
                     <Col xs={24} md={12}>
                         <Form.Item
                             name="name"
-                            label="Service Name"
+                            label="Product Name"
                             rules={[
                                 {
                                     required: true,
-                                    message: "Please enter service name",
+                                    message: "Please enter product name",
                                 },
                             ]}
                         >
-                            <Input placeholder="Enter service name" />
+                            <Input placeholder="Enter product name" />
                         </Form.Item>
                     </Col>
                 </Row>
 
-                <Row>
-                    <Col xs={24}>
-                        <Form.Item name="description" label="Description">
-                            <TextArea
-                                rows={4}
-                                placeholder="Enter service description"
+                <Row gutter={[12, 12]}>
+                    <Col xs={24} md={12}>
+                        <Form.Item
+                            name="rackRate"
+                            label="Rack Rate"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter rack rate",
+                                },
+                            ]}
+                            initialValue={0}
+                        >
+                            <InputNumber
+                                style={{ width: "100%" }}
+                                min={0}
+                                formatter={(value) =>
+                                    `₱ ${value}`.replace(
+                                        /\B(?=(\d{3})+(?!\d))/g,
+                                        ","
+                                    )
+                                }
+                                parser={(value) =>
+                                    value.replace(/₱\s?|(,*)/g, "")
+                                }
+                                placeholder="Enter amount"
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} md={12}>
+                        <Form.Item name="tag" label="Tags">
+                            <Select
+                                options={tagOptions}
+                                placeholder="Select tags"
+                                loading={tagsLoading}
                             />
                         </Form.Item>
                     </Col>
                 </Row>
 
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item name="description" label="Description">
+                            <TextArea
+                                rows={4}
+                                placeholder="Enter product description"
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item name="remarks" label="Remarks">
+                            <TextArea
+                                rows={4}
+                                placeholder="Enter product remarks"
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item
+                            name="status"
+                            label="Status"
+                            className="w-full"
+                        >
+                            <Radio.Group className="flex w-full gap-4">
+                                <Row gutter={[12, 12]}>
+                                    <Col span={12}>
+                                        <label className="flex items-center gap-2 w-full h-full cursor-pointer border rounded-md px-4 py-3 hover:border-blue-500 transition-all">
+                                            <Radio value={1} />
+                                            <span className="text-sm font-medium">
+                                                <p className="text-base font-semibold text-gray-800">
+                                                    Active
+                                                </p>
+                                                <p className="text-sm text-gray-500">
+                                                    Available to add to package
+                                                </p>
+                                            </span>
+                                        </label>
+                                    </Col>
+
+                                    <Col span={12}>
+                                        <label className="flex items-center gap-2 w-full h-full cursor-pointer border rounded-md px-4 py-3 hover:border-blue-500 transition-all">
+                                            <Radio value={0} />
+                                            <span className="text-sm font-medium">
+                                                <p className="text-base font-semibold text-gray-800">
+                                                    Inactive
+                                                </p>
+                                                <p className="text-sm text-gray-500">
+                                                    Not available to add to
+                                                    package
+                                                </p>
+                                            </span>
+                                        </label>
+                                    </Col>
+                                </Row>
+                            </Radio.Group>
+                        </Form.Item>
+                    </Col>
+                </Row>
             </Form>
         </>
     );
--- cfa-frontend/app/admin-dashboard/src/components/inventory/services/ServiceForm.jsx
+++ cfa-frontend/app/admin-dashboard/src/components/inventory/services/ServiceForm.jsx
@@ -1,10 +1,25 @@
-import { Form, Input, Button, Row, Col } from "antd";
+import { useEffect, useState } from "react";
+// Hooks
+import { useTags } from "../../../hooks/tag/useTags";
+// AntD
+import { Form, Input, InputNumber, Row, Col, Select, DatePicker, Radio } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
 
+const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 const ServiceForm = () => {
-    const [form] = Form.useForm();
+    const [serviceForm] = Form.useForm();
+
+    const [ tagOptions, setTagOptions ] = useState([]);
+
+    const { data: tags, isLoading: tagsLoading } = useTags();
+    
+    useEffect(() => {
+        if(tags && !tagsLoading) {
+            setTagOptions(tags?.data.map(tag => ({ label: tag.name, value: tag.id })));
+        }
+    }, [tags]);
 
     const handleSubmit = (values) => {
         console.log("Form values:", values);
@@ -12,14 +27,35 @@ const ServiceForm = () => {
     };
 
     const handleCancel = () => {
-        form.resetFields();
+        serviceForm.resetFields();
         // Handle cancel logic here
     };
 
     return (
         <>
-            <Form form={form} layout="vertical" onFinish={handleSubmit}>
-                <Row gutter={[16, 0]}>
+            <Form
+                form={serviceForm}
+                layout="vertical"
+                onFinish={handleSubmit}
+                requiredMark={false}
+            >
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item
+                            name="date"
+                            label="Availability Date"
+                            required
+                        >
+                            <RangePicker
+                                className="w-full"
+                                format="MMMM D, YYYY"
+                                placeholder={["Start Date", "End Date"]}
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row gutter={[12, 12]}>
                     <Col xs={24} md={12}>
                         <Form.Item
                             name="code"
@@ -51,7 +87,48 @@ const ServiceForm = () => {
                     </Col>
                 </Row>
 
-                <Row>
+                <Row gutter={[12, 12]}>
+                    <Col xs={24} md={12}>
+                        <Form.Item
+                            name="rackRate"
+                            label="Rack Rate"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter rack rate",
+                                },
+                            ]}
+                            initialValue={0}
+                        >
+                            <InputNumber
+                                style={{ width: "100%" }}
+                                min={0}
+                                formatter={(value) =>
+                                    `₱ ${value}`.replace(
+                                        /\B(?=(\d{3})+(?!\d))/g,
+                                        ","
+                                    )
+                                }
+                                parser={(value) =>
+                                    value.replace(/₱\s?|(,*)/g, "")
+                                }
+                                placeholder="Enter amount"
+                            />
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} md={12}>
+                        <Form.Item name="tag" label="Tags">
+                            <Select
+                                options={tagOptions}
+                                placeholder="Select tags"
+                                loading={tagsLoading}
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row gutter={[12, 12]}>
                     <Col xs={24}>
                         <Form.Item name="description" label="Description">
                             <TextArea
@@ -62,6 +139,59 @@ const ServiceForm = () => {
                     </Col>
                 </Row>
 
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item name="remarks" label="Remarks">
+                            <TextArea
+                                rows={4}
+                                placeholder="Enter service remarks"
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row gutter={[12, 12]}>
+                    <Col xs={24}>
+                        <Form.Item
+                            name="status"
+                            label="Status"
+                            className="w-full"
+                        >
+                            <Radio.Group className="flex w-full gap-4">
+                                <Row gutter={[12, 12]}>
+                                    <Col span={12}>
+                                        <label className="flex items-center gap-2 w-full h-full cursor-pointer border rounded-md px-4 py-3 hover:border-blue-500 transition-all">
+                                            <Radio value={1} />
+                                            <span className="text-sm font-medium">
+                                                <p className="text-base font-semibold text-gray-800">
+                                                    Active
+                                                </p>
+                                                <p className="text-sm text-gray-500">
+                                                    Available to add to package
+                                                </p>
+                                            </span>
+                                        </label>
+                                    </Col>
+
+                                    <Col span={12}>
+                                        <label className="flex items-center gap-2 w-full h-full cursor-pointer border rounded-md px-4 py-3 hover:border-blue-500 transition-all">
+                                            <Radio value={0} />
+                                            <span className="text-sm font-medium">
+                                                <p className="text-base font-semibold text-gray-800">
+                                                    Inactive
+                                                </p>
+                                                <p className="text-sm text-gray-500">
+                                                    Not available to add to
+                                                    package
+                                                </p>
+                                            </span>
+                                        </label>
+                                    </Col>
+                                </Row>
+                            </Radio.Group>
+                        </Form.Item>
+                    </Col>
+                </Row>
             </Form>
         </>
     );
